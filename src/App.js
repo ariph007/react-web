9acbc70162a2e360d274b52ea353b82fe7dd4c21
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2022-05-09 19:48:28 +0700

    Hero Section Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GlobalStyle from "./globalStyle";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-function App() {
+//*Page
+import Home from "./pages/Home";
+import Signup from "./pages/SignupPage";
+import Pricing from "./pages/PricingPage";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
